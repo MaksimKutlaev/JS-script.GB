85aee412e4b1d2405b5fbab02a41fb9b374d25fb
MaksimKutlaev <112340945+MaksimKutlaev@users.noreply.github.com>
2023-10-27 21:07:00 +0300

    add hw1-2

---
+++ scripts.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//1
+console.log("Текст из scripts.js");
+//2
+const num = 123;
+console.log(num);
+//3
+let a=5;
+a=7;
+console.log(a);
+//4
+let b;
+b=1+2+3;
+console.log(b);
+//5
+/* commit */
+
